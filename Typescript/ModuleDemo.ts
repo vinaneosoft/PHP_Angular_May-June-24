1edd442b4846b54f338c31db8e65c793d9dac77e
vina <vinaneosoft@gmail.com>
2024-05-24 13:13:41 +0530

    commit

--- Typescript/ModuleDemo.ts
+++ Typescript/ModuleDemo.ts
@@ -3,6 +3,8 @@ import { Department as CompanyDepartment} from "./Modules/MyCompany";
 
 import * as company from './Modules/MyCompany';
 
+import Trainer from './Modules/MyCollege'; // no curly braces, u can give alias  u can use same name
+
 let department1=new Department(123);
 console.log(department1.departmentId);
 department1.departmentId=777;
@@ -18,3 +20,6 @@ let companyDepartment1=new CompanyDepartment("PHP");
 
 let department2=new company.Department();
 console.log(company.companyName);
+
+let teacher1=new Trainer();
+teacher1.tid
--- Typescript/Modules/MyCollege.ts
+++ Typescript/Modules/MyCollege.ts
@@ -13,3 +13,11 @@ export let collegeName="LT College";
 export function setCollege(colName:string){
     collegeName=colName;
 }
+
+export default class Teacher{
+    tid:number;
+}
+//A module cannot have multiple default exports.
+/* export default class Student{
+} */
+
