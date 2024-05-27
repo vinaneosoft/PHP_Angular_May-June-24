2b5a8746bb344ea79b09752e1b5dae41f3bd0763
vina <vinaneosoft@gmail.com>
2024-05-27 12:23:49 +0530

    commit

---
+++ Typescript/Inheritance/MultipleNot.ts
@@ -0,0 +1,34 @@
+class Father{
+    dream():string{
+        return "Doctor";
+    }
+}
+class Mother{
+    dream():string{
+        return "Engineer"
+    }
+}
+class Child extends Father, Mother{
+    achiveDream(){
+        this.dream(); // ambiguity
+    }
+}
+// multiple inheritance via class in not supported
+
+interface Father{
+    dream():string;
+    hobbies():string;
+}
+interface Mother{
+    dream():string;
+    hobbies(what:string[]):string;
+}
+class Child2 implements Father, Mother{
+    dream():string{
+        return "Lawyer"
+    }
+    hobbies(): string {
+        return "Painiting";
+    }
+}
+// multiple inheritance via interfaces is supported with rules (if same method then must be identical)
