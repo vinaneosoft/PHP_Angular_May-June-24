c0c33bbe55514436e54d88bdac7601ac6054bbac
vina <vinaneosoft@gmail.com>
2024-05-27 14:34:51 +0530

    commit

--- Typescript/InterfaceDemo.ts
+++ Typescript/InterfaceDemo.ts
@@ -31,3 +31,69 @@ console.log(employee.getGrossSalary());
 employee=new NeoTrainer();
 employee.basicSalary=56000;
 console.log(employee.getGrossSalary());
+
+
+
+
+
+
+interface Queue{
+    insert(value:string):number; // inserting elements from back
+    remove():string;   // removing elements from front
+}
+interface Deque extends Queue{
+    insertFront(value:string):number; // inserting elements from front
+    removeBack():string // removing elements from from back
+}
+
+class DequeArray implements Deque{
+    insertFront(value: string): number {
+        throw new Error("Method not implemented.");
+    }
+    removeBack(): string {
+        throw new Error("Method not implemented.");
+    }
+    insert(value: string): number {
+        throw new Error("Method not implemented.");
+    }
+    remove(): string {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+class QueueArray implements Queue{
+    insert(value: string): number {
+        throw new Error("Method not implemented.");
+    }
+    remove(): string {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+// multiple inheritance is supported via interfaces with rules
+// rule: if method names are same then methods must be identical
+interface A{
+    test():void
+}
+interface B{
+    test():void;
+    demo():void
+}
+interface C extends A, B{
+    implement():void
+}
+
+class XYZ implements C{
+    implement(): void {
+        throw new Error("Method not implemented.");
+    }
+    test(): number {
+        throw new Error("Method not implemented.");
+    }
+    demo(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
