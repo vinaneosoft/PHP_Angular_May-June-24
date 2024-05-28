7e0faa2d669196b6236ae345c1bf2ee15194f861
vina <vinaneosoft@gmail.com>
2024-05-28 11:39:01 +0530

    commit

---
+++ Typescript/Generic/MapDemo.ts
@@ -0,0 +1,20 @@
+/*
+    key-value pair collection
+    unique collection (key must be unique, values can be duplicate)
+    non indexed collection
+ */
+
+    /* empid ==> Depatment code */
+    let map=new Map<number, string>();
+    map.set(123,"LD");
+    map.set(111,"JS")
+    map.set(333,"PHP")
+    map.set(123,"HR")
+    map.set(321,"Java")
+    map.set(567,"LD")
+    map.set(567,"JS")
+    console.log(map);
+    
+    let itr=map.entries();
+    console.log(itr);
+    
