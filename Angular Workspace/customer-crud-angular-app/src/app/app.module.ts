a2c22561703e37184e1c8e82526ed2854c4f4d86
vina <vinaneosoft@gmail.com>
2024-05-28 13:21:07 +0530

    commit

---
+++ Angular Workspace/customer-crud-angular-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+/* applied above class, meta data about this class Module class 
+What this module contains  */
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
