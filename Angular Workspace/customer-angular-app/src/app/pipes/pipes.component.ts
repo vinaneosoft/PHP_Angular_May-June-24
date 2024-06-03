d7cea14b32d5195828a39d95ea087c03152addbe
vina <vinaneosoft@gmail.com>
2024-06-03 13:19:23 +0530

    commit

--- Angular Workspace/customer-angular-app/src/app/pipes/pipes.component.ts
+++ Angular Workspace/customer-angular-app/src/app/pipes/pipes.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
-  productPrices=[12789.45, 36456.678,32100.5555,678.78787, 345, 4555.5,78000,678990]
+  productPrices=[12789.45, 36456.678,32100.5555,678.78787, 345, 4555.5,78000,678990.141421]
+  sysDate=new Date();
+  myDate=new Date('Jan 1, 2024');
 }
