6b6df0fcf71286784032c7fdd5c9f29a15e0120e
vina <vinaneosoft@gmail.com>
2024-06-04 11:26:36 +0530

    commit

--- Angular Workspace/customer-angular-app/src/app/custompipes/orderby.pipe.ts
+++ Angular Workspace/customer-angular-app/src/app/custompipes/orderby.pipe.ts
@@ -5,12 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class OrderbyPipe implements PipeTransform {
 
-  transform(originalarray: any, ...args: string[]): any {
-
-    // sort logic
-    // sorting custom array
-
-    return originalarray;
+  transform(originalarray: any[], ...args: string[]): any {
+   // console.log(originalarray);
+    const property=args[0]; //id
+  //  console.log(property);
+    // string[], 
+    //number[], object[]
+    return originalarray.sort((ob1:any,ob2:any)=>{
+        if(ob1[property]>ob2[property]) //ob1.id>ob2.id
+          return 1;
+        else if(ob1[property]<ob2[property])
+          return -1
+        else
+          return 0;
+    });
+    
   }
 
 }
