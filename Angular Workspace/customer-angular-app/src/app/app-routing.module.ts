1f2be3e2fb3bff58695569768fb801f40678ce77
vina <vinaneosoft@gmail.com>
2024-06-05 17:02:53 +0530

    commit

--- Angular Workspace/customer-angular-app/src/app/app-routing.module.ts
+++ Angular Workspace/customer-angular-app/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { CustomersComponent } from './customers/customers.component';
 
 const routes: Routes = [
   {
+    path:'',
+    redirectTo:'home',
+    pathMatch: 'full'
+  },
+  {
     path:'home',
     component:HomeComponent
   },
