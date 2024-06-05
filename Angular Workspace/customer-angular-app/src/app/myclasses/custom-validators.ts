6c42704c578aeb2035e710da33bac3bb4312fdb3
vina <vinaneosoft@gmail.com>
2024-06-05 13:06:01 +0530

    commit

--- Angular Workspace/customer-angular-app/src/app/myclasses/custom-validators.ts
+++ Angular Workspace/customer-angular-app/src/app/myclasses/custom-validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl, FormGroup } from "@angular/forms";
 
 export class CustomValidators {
     static compare(group:AbstractControl):{}|null{
-        const pass=group.value.password;
+        const pass=group.value.password; //group.get("password")?.value
         const cpass=group.value.cpassword;
         if(pass!=cpass)
                 return {compare:true}
