85babc1f7c79e26a9ed4b74870d94bb91e6eba30
vina <vinaneosoft@gmail.com>
2024-06-06 14:34:02 +0530

    commit

---
+++ Angular Workspace/customer-angular-app/src/app/myservices/counter.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  serviceCounter=0;
+  constructor() { }
+
+  incrementCounter():void{
+    this.serviceCounter++;
+  }
+  getCounter():number{
+    return this.serviceCounter;
+  }
+}
